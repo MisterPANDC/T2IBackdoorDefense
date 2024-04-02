9b492c2f83bf1e343e68e557f73d491d583b1369
YifeiChen <akk879@outlook.com>
2024-04-02 22:11:42 +0800

    Update xformers

--- inference.py
+++ inference.py
@@ -8,8 +8,10 @@ from utils import get_hf_name
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--device', type=str, default='cuda:0')
-parser.add_argument('--model', type=str, default='sd_v1.4', choices=['sd_v1.4', 'sd_v1.5', 'sd_v2.1'])
+parser.add_argument('--model', type=str, default='sd_v1.5', choices=['sd_v1.4', 'sd_v1.5', 'sd_v2.1'])
 parser.add_argument('--fp16', type=bool , default=True)
+parser.add_argument('--xformers', type=bool, default=True)
+parser
 
 args = parser.parse_args()
 
@@ -22,10 +24,14 @@ else:
     pipe = StableDiffusionPipeline.from_pretrained(hf_name)
 pipe.to(args.device)
 
-# Input Prompt
-while(True):
-    prompt = input("Enter Prompt: ")
-    images = pipe(prompt).images[0]
+if args.xformers:
+    pipe.enable_xformers_memory_efficient_attention()
 
-    # Save Image
-    images.save("output.png")
+with torch.inference_mode():
+    # Input Prompt
+    while(True):
+        prompt = input("Enter Prompt: ")
+        images = pipe(prompt).images[0]
+
+        # Save Image
+        images.save("output.png")
